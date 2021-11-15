fb0fdf248bdc2a50ee9707b569d73a51e15bc39f
xiaohh <1296522554@qq.com>
2021-11-15 15:42:08 +0800

    完成 Chart12

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,8 @@ import {Chart7} from '../components/chart-7';
 import {Chart8} from '../components/chart-8';
 import {Chart9} from '../components/chart-9';
 import {Chart10} from '../components/chart-10';
+import {Chart11} from '../components/chart-11';
+import {Chart12} from '../components/chart-12';
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -45,12 +47,13 @@ export const Home = () => {
                         <h2>案发类型统计</h2>
                         <div className="charts">
                             <Chart10/>
+                            <Chart11/>
                         </div>
                     </div>
                     <div className="bordered row2 案发街道">
                         <h2>案发街道统计</h2>
                         <div className="charts">
-
+                            <Chart12/>
                         </div>
                     </div>
                     <div className="bordered row3 作案手段">
